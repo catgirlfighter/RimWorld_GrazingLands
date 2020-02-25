1adfa37f54afe07439cf0a867c0555cafaa59966
CGFighter <catgirlfighter@gmail.com>
2020-02-25 23:24:13 +0300

    RW 1.1

---
+++ source/GrazingLands10/Settings.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Harmony;
+using UnityEngine;
+using Verse;
+
+namespace GrazingLands
+{
+    public class Settings : ModSettings
+    {
+        public static float multiplier = 0f;
+
+        public static void DoSettingsWindowContents(Rect inRect)
+        {
+            Listing_Standard listing_Standard = new Listing_Standard();
+            listing_Standard.Begin(inRect);
+            listing_Standard.Label("multiplierLabel".Translate((100 + Math.Round(multiplier, 3) * 400).ToString()));
+            multiplier = listing_Standard.Slider(multiplier, 0f, 1f);
+            listing_Standard.End();
+        }
+
+        public override void ExposeData()
+        {
+            base.ExposeData();
+            Scribe_Values.Look(ref multiplier, "multiplier", 0f, false);
+        }
+    }
+}
