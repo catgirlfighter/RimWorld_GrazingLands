552f7216eed1d0d95d796b55616fa968a5bf5299
CGFighter <catgirlfighter@gmail.com>
2021-07-08 01:54:19 +0300

    1.1.7

---
+++ source/GrazingLands13/GrazingLands.cs
@@ -0,0 +1,34 @@
+﻿using HarmonyLib;
+using System.Reflection;
+using Verse;
+
+namespace GrazingLands
+{
+    [StaticConstructorOnStartup]
+    public class GrazingLands : Mod
+    {
+        public static Settings Settings;
+
+        public GrazingLands(ModContentPack content) : base(content)
+        {
+            var harmony = new Harmony("net.avilmask.rimworld.mod.GrazingLands");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+            GetSettings<Settings>();
+        }
+
+        public void Save()
+        {
+            LoadedModManager.GetMod<GrazingLands>().GetSettings<Settings>().Write();
+        }
+
+        public override string SettingsCategory()
+        {
+            return "GrazingLands";
+        }
+
+        public override void DoSettingsWindowContents(UnityEngine.Rect inRect)
+        {
+            Settings.DoSettingsWindowContents(inRect);
+        }
+    }
+}
