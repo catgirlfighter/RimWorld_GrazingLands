5e80484951d04881456cd56df081f3e68500d6d4
p.tychinin <catgirlfighter@gmail.com>
2024-04-06 17:13:24 +0300

    1.2.0

---
+++ source/GrazingLands15/PlantPropertiesPatch.cs
@@ -0,0 +1,119 @@
+﻿using HarmonyLib;
+using RimWorld;
+using Verse;
+using UnityEngine;
+
+namespace GrazingLands
+{
+    class PlantPropertiesPatch
+    {
+        static float RoundUp(float val)
+        {
+            if (val - Mathf.Floor(val) > 0)
+                val = Mathf.Floor(val) + 1f;
+            else
+                val = Mathf.Floor(val);
+            return val;
+        }
+
+        [HarmonyPatch(typeof(RimWorld.Plant), "IngestedCalculateAmounts")]
+        static class Plant_IngestedCalculateAmounts_GrazingLandsPatch
+        {
+            public static bool Prefix(ref Plant __instance, Pawn ingester, float nutritionWanted, out int numTaken, out float nutritionIngested)
+            {
+                if (__instance.def.plant.harvestedThingDef != null && !__instance.def.plant.harvestedThingDef.IsNutritionGivingIngestible)
+                {
+                    numTaken = 0;
+                    nutritionIngested = 0f;
+                    return true;
+                }
+
+                bool hasyield = __instance.def.plant.harvestYield > 0 && __instance.def.plant.harvestedThingDef != null;
+
+                bool WildOrNonCrop = (!__instance.def.plant.Sowable || __instance.def.plant.harvestedThingDef == null) && __instance.def.IsIngestible && __instance.def.ingestible.foodType == FoodTypeFlags.Plant;
+                bool HarvestDestroys = __instance.def.plant.HarvestDestroys && !WildOrNonCrop;
+
+                float maxAmount;
+                float harvestYield;
+                float nutrition;
+
+                if (hasyield)
+                {
+                    harvestYield = __instance.def.plant.harvestYield;
+                    nutrition = __instance.def.plant.harvestedThingDef.ingestible.CachedNutrition;
+                    maxAmount = RoundUp(harvestYield * Mathf.Lerp(0.5f, 1f, __instance.HitPoints / __instance.MaxHitPoints));
+                }
+                else
+                {
+                    harvestYield = 100;
+                    nutrition = __instance.GetStatValue(StatDefOf.Nutrition, false) / harvestYield * Settings.Multiplier;
+                    if (HarvestDestroys)
+                        maxAmount = RoundUp(harvestYield * Mathf.Lerp(0.5f, 1f, __instance.HitPoints / __instance.MaxHitPoints));
+                    else
+                        maxAmount = RoundUp(harvestYield * __instance.Growth);
+                }
+
+                float needAmount = RoundUp(nutritionWanted / nutrition);
+
+                if (HarvestDestroys)
+                {
+                    maxAmount = Mathf.Min(maxAmount, needAmount);
+                    nutritionIngested = maxAmount * nutrition;
+
+                    float potentialDamage = Mathf.Lerp(0f, 1f, maxAmount / harvestYield);
+                    potentialDamage = __instance.MaxHitPoints * potentialDamage * Settings.YieldDamage;
+                    if (__instance.HitPoints - potentialDamage <= 0)
+                    {
+                        numTaken = 1;
+                    }
+                    else
+                    {
+                        numTaken = 0;
+                        __instance.TakeDamage(new DamageInfo(DamageDefOf.Bite, potentialDamage, 0f, -1f, null, null, null, DamageInfo.SourceCategory.ThingOrUnknown, null));
+                    }
+                }
+                else
+                {
+                    numTaken = 0;
+
+                    if (hasyield)
+                    {
+                        maxAmount = Mathf.Min(maxAmount, needAmount * Settings.YieldDamage);
+                        nutritionIngested = maxAmount / Settings.YieldDamage * nutrition;
+                    } 
+                    else
+                    {
+                        maxAmount = Mathf.Min(maxAmount, needAmount);
+                        nutritionIngested = maxAmount * nutrition;
+                    }
+
+                    float potentialDamage = Mathf.Lerp(0f, 1f, maxAmount / harvestYield);
+                    __instance.Growth -= potentialDamage;
+                    if (__instance.Growth < 0.08f)
+                    {
+                        if (!hasyield && Settings.ConsumeChance > 0)
+                            if (Settings.ConsumeChance == 100)
+                                numTaken = 1;
+                            else
+                            {
+                                _ = new Random();
+                                int val = Random.Range(1, 100);
+                                if (val <= Settings.ConsumeChance)
+                                    numTaken = 1;
+                            }
+
+                            if (numTaken == 0)
+                                __instance.Growth = 0.08f;
+                    }
+
+                    if (__instance.Spawned)
+                    {
+                        __instance.Map.mapDrawer.MapMeshDirty(__instance.Position, MapMeshFlagDefOf.Things);
+                    }
+                }
+
+                return false;
+            }
+        }
+    }
+}
---
+++ source/GrazingLands15/Settings.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+using Verse;
+
+namespace GrazingLands
+{
+    public class Settings : ModSettings
+    {
+        private static float multiplier = 0f;
+        private static float consumeChance = 0f;
+        private static float yieldDamage = 0f;
+
+        public static float Multiplier { get { return 1 + multiplier * 4; } }
+        public static int ConsumeChance { get { return (int)Math.Round(consumeChance * 100); } }
+        public static float YieldDamage { get { return 1 + yieldDamage * 4; } }
+
+        public static void DoSettingsWindowContents(Rect inRect)
+        {
+            Listing_Standard listing_Standard = new Listing_Standard();
+            listing_Standard.Begin(inRect);
+            listing_Standard.Label("multiplierLabel".Translate((100 + Math.Round(multiplier, 3) * 400).ToString()));
+            multiplier = listing_Standard.Slider(multiplier, 0f, 1f);
+            listing_Standard.Label("consumeChanceLabel".Translate(Math.Round(consumeChance * 100).ToString()));
+            consumeChance = listing_Standard.Slider(consumeChance, 0f, 1f);
+            listing_Standard.Label("yieldDamageLabel".Translate((100 + Math.Round(yieldDamage, 3) * 400).ToString()));
+            yieldDamage = listing_Standard.Slider(yieldDamage, 0f, 1f);
+            listing_Standard.End();
+        }
+
+        public override void ExposeData()
+        {
+            base.ExposeData();
+            Scribe_Values.Look(ref multiplier, "multiplier", 0f, false);
+            Scribe_Values.Look(ref consumeChance, "consumeChance2", 0f, false);
+            Scribe_Values.Look(ref yieldDamage, "yieldDamage", 0.5f, false);
+        }
+    }
+}
